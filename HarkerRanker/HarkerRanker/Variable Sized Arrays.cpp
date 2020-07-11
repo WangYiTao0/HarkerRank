8bb722235bc8205328d8ce5142ef647b501c76d7
W <as5818956@gmail.com>
2020-07-11 21:05:02 +0900

    Variable Sized Arrays.cpp

---
+++ HarkerRanker/HarkerRanker/Variable Sized Arrays.cpp
@@ -0,0 +1,44 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+	/* Enter your code here. Read input from STDIN. Print output to STDOUT */
+	int rowNum, questionNum;
+	cin >> rowNum >> questionNum;
+
+	vector<int> arr;
+	vector<vector<int>> arrNum;
+	int dataSize;
+	int data;
+	for (int row = 0; row < rowNum; row++)
+	{
+		cin >> dataSize;
+		for (int size = 0; size < dataSize; size++)
+		{
+			cin >> data;
+			arr.push_back(data);
+		}
+		arrNum.push_back(arr);
+		arr.clear();
+	}
+
+	int i, j;
+	vector<int> result;
+
+	for (int q = 0; q < questionNum; q++)
+	{
+		cin >> i >> j;
+		result.push_back(arrNum[i][j]);
+	}
+
+	for (auto r : result)
+	{
+		cout << r << endl;
+	}
+	return 0;
+}
