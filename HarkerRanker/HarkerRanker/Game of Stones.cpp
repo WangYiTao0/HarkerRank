7d7b6829bded29ce32886288afe7f3421ef47802
W <as5818956@gmail.com>
2020-07-13 10:32:42 +0900

    save

---
+++ HarkerRanker/HarkerRanker/Game of Stones.cpp
@@ -0,0 +1,33 @@
+//#include <iostream>
+//#include <fstream>
+//
+//using namespace std;
+//
+//// Complete the gameOfStones function below.
+//string gameOfStones(int n) {
+//
+//
+//}
+//
+//int main()
+//{
+//	ofstream fout(getenv("OUTPUT_PATH"));
+//
+//	int t;
+//	cin >> t;
+//	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+//
+//	for (int t_itr = 0; t_itr < t; t_itr++) {
+//		int n;
+//		cin >> n;
+//		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+//
+//		string result = gameOfStones(n);
+//
+//		fout << result << "\n";
+//	}
+//
+//	fout.close();
+//
+//	return 0;
+//}
---
+++ HarkerRanker/HarkerRanker/time conversion.cpp
@@ -0,0 +1,60 @@
+﻿//#include <string>
+//#include <sstream>
+//#include <iostream>
+//#include <fstream>
+//
+//using namespace std;
+//
+///*
+// * Complete the timeConversion function below.
+// */
+//string timeConversion(string s) {
+//    /*
+//     * Write your code here.
+//     */
+//    string tmptime(s);
+//    int h_time = 0;
+//    //tmptime.replace(2,s.length()-2,"");
+//    tmptime.erase(2, s.length() - 2);
+//    stringstream ss(tmptime);
+//    ss >> h_time; //get hour
+//
+//    if (s.find("AM")!= -1)
+//    {
+//        //s.replace(s.length()-2,2,"");
+//        s.erase(s.length() - 2, 2);
+//        if (h_time == 12)
+//            s.replace(0, 2, "00");
+//    }
+//
+//    if (s.find("PM") != -1)
+//    {
+//        //s.replace(s.length()-2,2,"");
+//        s.erase(s.length() - 2, 2);
+//        if (h_time != 12)
+//            h_time += 12;
+//        stringstream iss;
+//        iss << h_time;
+//        iss >> tmptime;
+//        s.replace(0, 2, tmptime);
+//    }
+//    return s;
+//
+//}
+//
+//int main()
+//{
+//   // ofstream fout(getenv("OUTPUT_PATH"));
+//
+//    string s;
+//    getline(cin, s);
+//
+//    string result = timeConversion(s);
+//
+//    cout << result << endl;
+//   // fout << result << "\n";
+//
+//    //fout.close();
+//
+//    return 0;
+//}
