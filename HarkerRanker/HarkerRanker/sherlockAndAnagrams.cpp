6c4ba56b71e38946a3489f3e8fb9f0a34e46df6e
W <as5818956@gmail.com>
2020-07-19 14:14:26 +0900

    /Zc:__cplusplus 　℃++ option
    anagram 2

--- HarkerRanker/HarkerRanker/sherlockAndAnagrams.cpp
+++ HarkerRanker/HarkerRanker/sherlockAndAnagrams.cpp
@@ -1,19 +1,69 @@
 #include <bits/stdc++.h>
-#include <unordered_map>
+
 using namespace std;
+
+template <class T>
+inline void hash_combine(std::size_t& seed, const T& v)
+{
+	std::hash<T> hasher;
+	seed ^= hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+}
+
+class Signatrue
+{
+public:
+	void Push(char c)
+	{
+		counts[c - 'a']++;
+	}
+	bool operator ==(const Signatrue& rhs)const
+	{
+		return counts == rhs.counts;
+	}
+	size_t Hash() const
+	{
+		size_t hash = 0;
+		for (auto v : counts)
+		{
+			hash_combine(hash, v);
+		}
+		return hash;
+	}
+private:
+	array<char, 26> counts{};
+};
+
+namespace std {
+	template<> struct hash<Signatrue>
+	{
+		size_t operator()(const Signatrue& s)const {
+			return s.Hash();
+		}
+	};
+};
 
 class AnagramMap
 {
 public:
-	void push(string s) noexcept
+	// void push(string s) noexcept
+	// {
+	//     sort(s.begin(),s.end());
+	//     auto r = map.emplace(std::move(s),1);
+	//     if(!r.second)
+	//     {
+	//         r.first->second++;
+	//     }
+	// }
+	void push(const Signatrue& sig) noexcept
 	{
-		sort(s.begin(), s.end());
-		auto r = map.emplace(std::move(s), 1);
+		auto r = map.emplace(sig, 1);
 		if (!r.second)
 		{
 			r.first->second++;
 		}
 	}
+
+
 	int Count() const noexcept
 	{
 		return  accumulate(map.begin(), map.end(), 0, [](auto& acc, auto& v) {
@@ -21,7 +71,8 @@ public:
 			});
 	}
 private:
-	unordered_map<string, int> map;
+	//unordered_map<string, int> map;
+	unordered_map<Signatrue, int> map;
 };
 
 // Complete the sherlockAndAnagrams function below.
@@ -29,12 +80,11 @@ int sherlockAndAnagrams(string s) {
 	AnagramMap map;
 	for (auto i = s.begin(); i < s.end(); ++i)
 	{
-//		auto a = i;
+		Signatrue sig;
 		for (auto j = i; j < s.end(); j++)
 		{
-			map.push(string(i, j + 1));
-			//auto b = j+1;
-			//auto s = string(i, j + 1);
+			sig.Push(*j);
+			map.push(sig);
 		}
 	}
 	return map.Count();
